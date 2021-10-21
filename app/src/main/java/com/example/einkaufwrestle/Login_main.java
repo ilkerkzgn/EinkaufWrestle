c89799bfceaebce80063a06816aab5fd7b8690b1
ilkerkzgn <kzgnlker@gmail.com>
2021-10-21 23:52:45 +0200

    Integration of the Firebase, Google Play Services, Google Services.

--- app/src/main/java/com/example/einkaufwrestle/Login_main.java
+++ app/src/main/java/com/example/einkaufwrestle/Login_main.java
@@ -1,11 +1,26 @@
 package com.example.einkaufwrestle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
 public class Login_main extends AppCompatActivity {
+
+
+    private static final String TAG = "GoogleActivity";
+    private static final int RC_SIGN_IN = 9001;
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +28,25 @@ public class Login_main extends AppCompatActivity {
         setContentView(R.layout.activity_login_main);
 
 
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("576151217475-ehq162ug1h638p9ghii1d4p3vhqlho6k.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+    }
+
+
+    //SIGN IN BUTTON - LOGIN PAGE
+    public void signIn (){
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+
+
+
     }
 }
+
