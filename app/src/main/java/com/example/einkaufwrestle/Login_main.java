ecfefbee72510e070c81132bf39cc34d95fdb63a
ilkerkzgn <kzgnlker@gmail.com>
2021-10-21 22:37:21 +0200

    Design wise, fix for constraint problems.

--- app/src/main/java/com/example/einkaufwrestle/Login_main.java
+++ app/src/main/java/com/example/einkaufwrestle/Login_main.java
@@ -3,6 +3,7 @@ package com.example.einkaufwrestle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class Login_main extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class Login_main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_main);
+
+
     }
 }
